9933c9e7fd954fddfd824cbe459b0337b00b3933
kkokwara2014 <kkokwara2014@gmail.com>
2019-09-19 18:27:27 +0100

    --- Auto-Git Commit ---

--- app/Department.php
+++ app/Department.php
@@ -4,7 +4,13 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+use App\User;
+
 class Department extends Model
 {
-    //
+    protected $fillable=['name','code'];
+
+    public function user(){
+        return $this->hasMany(User::class);
+    }
 }
--- app/Project_user.php
+++ app/Project_user.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Project_user extends Model
 {
-    //
+    public function project(){
+        return $this->belongsToMany(Project::class,'project_users')->withTimestamps();
+    }
+    public function user(){
+        return $this->belongsToMany(User::class,'project_users')->withTimestamps();
+    }
 }
