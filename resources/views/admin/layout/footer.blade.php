4c5d29a579acc0b52d155fa91a5fd2b7f0749bd0
kkokwara2014 <kkokwara2014@gmail.com>
2019-10-08 13:46:22 +0100

    --- Auto-Git Commit ---

--- resources/views/admin/layout/footer.blade.php
+++ resources/views/admin/layout/footer.blade.php
@@ -203,20 +203,6 @@
 <!-- ./wrapper -->
 
 <!-- jQuery 3 -->
-{{-- <script src="{{asset('admin_assets/bower_components/jquery/dist/jquery.min.js')}}"></script> --}}
-<!-- jQuery UI 1.11.4 -->
-{{-- <script src="{{asset('admin_assets/bower_components/jquery-ui/jquery-ui.min.js')}}"></script> --}}
-<!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
-{{-- <script> --}}
-  {{-- $.widget.bridge('uibutton', $.ui.button); --}}
-{{-- </script> --}}
-<!-- Bootstrap 3.3.7 -->
-{{-- <script src="{{asset('admin_assets/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script> --}}
-
-<!-- Select2 -->
-{{-- <script src="{{asset('admin_assets/bower_components/select2/dist/js/select2.full.min.js')}}"></script> --}}
-
-<!-- jQuery 3 -->
 <script src="{{asset('admin_assets/bower_components/jquery/dist/jquery.min.js')}}"></script>
 <!-- Bootstrap 3.3.7 -->
 <script src="{{asset('admin_assets/bower_components/bootstrap/dist/js/bootstrap.min.js')}}"></script>
